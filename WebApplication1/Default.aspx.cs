ca9feff8fba0c7f1dbd7d6ccc3f1719827587ee1
emilgelm <emilgelm@EMILGELM-MOBL1>
2017-04-09 16:25:18 +0300

    Add project files.

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WindowsIdentity identity = HttpContext.Current.Request.LogonUserIdentity;
+            string userName = identity.Name;
+           string[] test =userName.Split('\\');
+            userName = test[1];
+            username.Text = "Hello " +userName;
+
+        }
+    }
+}
